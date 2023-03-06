6e0ec670ec4ebc1ac7992dd9b3fa95221083392a
Ainsley Alceme <youngchill101@gmail.com>
2023-03-06 14:10:48 -0500

    submits

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+    
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+  };
+
+  function destructivelyPrependCat(){
+    cats.unshift('Bob')
+  }
+   
+  function destructivelyRemoveLastCat(){
+    cats.pop()
+  }
+  function destructivelyRemoveFirstCat(){
+    cats.shift()
+  }
+function appendCat(){
+    let newArray = [...cats,"Broom"]
+    return newArray
+}
+
+function prependCat(){
+let newArray = ["Arnold", ...cats]
+return newArray
+
+}function removeLastCat(){
+    let newArray = cats.slice(0,cats.length-1)
+    return newArray
+}
+function removeFirstCat(){
+    let newArray = cats.slice(1)
+    return newArray
+}
